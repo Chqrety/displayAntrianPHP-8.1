6bfe7a92c4b4a1002bc92d73141bfec11b515e2a
Chqrety <fikriaverus23@gmail.com>
2024-06-25 23:54:15 +0700

    build: add routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DisplayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('antrian/tv', [DisplayController::class, 'getData'])->name('antrian.tv.get');
+Route::post('antrian/tv', [DisplayController::class, 'data'])->name('antrian.tv.data');
+Route::delete('/antrian/tv', [DisplayController::class, 'deleteData'])->name('antrian.tv.delete');
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AntrianController;
+use App\Http\Controllers\DisplayController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/* route full
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::get('/siswa/{id}/show', [SiswaController::class, 'show'])->name('siswa.show');
+Route::get('/siswa/{id}/edit', [SiswaController::class, 'edit'])->name('siswa.edit');
+Route::patch('/siswa/{id}', [SiswaController::class, 'update'])->name('siswa.update');
+Route::post('/siswa/store', [SiswaController::class, 'store'])->name('siswa.store');
+Route::delete('/siswa/destroy/{id}', [SiswaController::class, 'destroy'])->name('siswa.destroy');
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/antrian', [AntrianController::class, 'index'])->name('antrian.index');
+Route::get('/antrian/data', [AntrianController::class, 'data'])->name('antrian.data');
+Route::post('/antrian/store', [AntrianController::class, 'store'])->name('antrian.store');
+Route::post('/antrian/generate', [AntrianController::class, 'generate'])->name('antrian.generate');
+
+Route::get('/display', [DisplayController::class, 'index'])->name('display.index');
