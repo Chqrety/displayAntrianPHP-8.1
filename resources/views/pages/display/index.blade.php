73d5cbb99d4b5ef7ad652fe19fa4d6efce9e929d
Chqrety <fikriaverus23@gmail.com>
2024-06-26 12:25:28 +0700

    feat: add logo and change no_antrian position

--- resources/views/pages/display/index.blade.php
+++ resources/views/pages/display/index.blade.php
@@ -11,14 +11,24 @@
         </div>
         <div class="flex h-full">
             <div class="flex w-full flex-col">
-                <div class="bg-gradient-to-r from-black/50 px-5 py-5">
-                    <span class="text-8xl font-bold text-white">LOGO</span>
+                <div class="flex h-36 bg-gradient-to-r from-black/50 px-5 py-5">
+                    <div class="flex w-fit items-center gap-3">
+                        <img class="h-full" src="{{ asset('assets/images/logo_ropanasuri.png') }}" alt="logo_ropanasuri">
+                        <div class="flex w-full flex-col">
+                            <span class="text-4xl text-[#34c4e7]">ROPANASURI</span>
+                            <span class="text-xl text-[#aad26c]">RUMAH SAKIT KHUSUS BEDAH</span>
+                        </div>
+                    </div>
                 </div>
                 <div class="h-full">
                     <div class="grid h-full grid-cols-3 grid-rows-2 gap-2 p-5">
-                        <div class="flex items-center justify-center rounded-lg bg-black/30">
-                            <span class="text-8xl font-bold text-gray-50">LOKET <span
-                                    class="text-yellow-300">C</span></span>
+                        <div class="flex flex-col gap-2 rounded-lg bg-black/30 px-5 py-2 text-center">
+                            <div>
+                                <span class="text-xl font-bold text-white" id="text_no_antrian">NOMOR ANTRIAN</span>
+                            </div>
+                            <div class="bg-white/15 flex h-5/6 flex-col items-center justify-center rounded-lg text-white"
+                                id="no_antrian_display">
+                            </div>
                         </div>
                         <div class="col-span-2 row-span-2 flex flex-col rounded-lg bg-black/30">
                             <div class="w-full p-2">
@@ -29,17 +39,17 @@
                                     <div class="flex h-full w-full items-center justify-around bg-white/5 text-white">
                                         <span class="text-7xl font-medium" id="no_antrian_{{ $j }}"></span>
                                         <span class="text-7xl font-medium" id="no_antrian_rm_{{ $j }}"></span>
-                                        <span class="text-3xl font-medium" id="no_poli_{{ $j }}"></span>
+                                        <span class="text-6xl font-medium" id="no_poli_{{ $j }}"></span>
                                     </div>
                                 @endfor
                             </div>
                         </div>
                         <div class="flex flex-col gap-2 rounded-lg bg-black/30 px-5 py-2 text-center">
                             <div>
-                                <span class="text-xl font-bold text-white" id="text_no_antrian">NOMOR ANTRIAN</span>
+                                <span class="text-xl font-bold text-white" id="text_no_antrian">MENUJU</span>
                             </div>
-                            <div class="bg-white/15 flex h-5/6 flex-col items-center justify-center rounded-lg text-white"
-                                id="no_antrian_display">
+                            <div class="flex h-5/6 flex-col items-center justify-center rounded-lg text-white"
+                                id="tujuan_display">
                             </div>
                         </div>
                     </div>
@@ -107,7 +117,7 @@
         var urlAPI = base_url + '/api/antrian/tv';
         let previousTimestamp = 0;
 
-        // window.addEventListener('load', function() {
+        // window.addEventListener('load', function() {     //aktifkan jika ingin melakukan penghapusan data dengan cara reload pages
         //     // Fetch untuk menghapus data pada API saat halaman dimuat ulang
         //     fetch(urlAPI, {
         //             method: 'DELETE',
@@ -154,68 +164,6 @@
         // Panggil startPolling() untuk memulai polling
         startPolling();
 
-        // Fungsi untuk mengupdate jumlah data yang telah masuk
-        // function updateDataCount() {
-        //     const countElement = document.getElementById('count_riwayat');
-        //     console.log(countElement);
-        //     let dataCount = parseInt(countElement.textContent) || 0;
-        //     dataCount++;
-        //     countElement.textContent = dataCount;
-        // }
-
-        // Contoh panggilan updateDataCount() ketika data baru diterima
-        // Misalnya, setelah berhasil memperbarui UI dengan data baru
-        // updateDataCount();
-
-        // function updateNoAntrianDisplay(data) {
-        //     const latestData = data[0];
-        //     const firstFiveData = data.slice(0, 5);
-        //     firstFiveData.forEach((item, index) => {
-        //         item.id = index + 1; // Menambahkan properti id ke setiap elemen data
-
-        //         // Dapatkan elemen div
-        //         const riwayat_antrian = document.getElementById('riwayat_antrian');
-        //         const noAntrianDisplay = document.getElementById('no_antrian_display');
-
-        //         if (latestData.status === "poli") {
-        //             const noAntrianSpan = document.getElementById(`no_antrian_${item.id}`);
-        //             const noAntrianRMSpan = document.getElementById(`no_antrian_rm_${item.id}`);
-        //             const noPoliSpan = document.getElementById(`no_poli_${item.id}`);
-
-
-        //             if (noAntrianSpan && noAntrianRMSpan && noPoliSpan) {
-        //                 noAntrianSpan.textContent = item.no_antrian;
-        //                 noAntrianRMSpan.textContent = item.no_antrian_rm;
-        //                 noPoliSpan.textContent = item.no_poli;
-        //             }
-
-        //             noAntrianDisplay.innerHTML = `
-    //         <span id='no_antrian_latest' class="text-9xl font-bold text-yellow-300">${latestData.no_antrian}</span>
-    //         <span id='no_poli_latest' class="text-3xl font-medium">${latestData.no_poli}</span>
-    //         `;
-
-        //         } else if (latestData.status === "rekam medis") {
-        //             const noAntrianSpan = document.getElementById(`no_antrian_${item.id}`);
-        //             const noAntrianRMSpan = document.getElementById(`no_antrian_rm_${item.id}`);
-        //             const noPoliSpan = document.getElementById(`no_poli_${item.id}`);
-
-
-        //             if (noAntrianSpan && noAntrianRMSpan && noPoliSpan) {
-        //                 noAntrianSpan.textContent = item.no_antrian;
-        //                 noAntrianRMSpan.textContent = item.no_antrian_rm;
-        //                 noPoliSpan.textContent = item.no_poli;
-        //             }
-        //             noAntrianDisplay.innerHTML = `
-    //         <span id='no_antrian_rm_latest' class="text-9xl font-bold text-yellow-300">${latestData.no_antrian_rm}</span>
-    //         `;
-        //         } else {
-        //             noAntrianDisplay.innerHTML = `
-    //         <span class="text-xl font-medium">Tidak ada status</span>
-    //         `;
-        //         }
-        //     });
-        // }
-
         function updateNoAntrianDisplay(data) {
             // Ambil 5 data unik dari data yang diterima
             const uniqueData = Array.from(new Set(data.map(item => item.no_antrian + item.no_antrian_rm + item.no_poli)))
@@ -228,6 +176,7 @@
             // Dapatkan elemen div
             const riwayat_antrian = document.getElementById('riwayat_antrian');
             const noAntrianDisplay = document.getElementById('no_antrian_display');
+            const tujuanDisplay = document.getElementById('tujuan_display');
 
             if (!latestData) {
                 noAntrianDisplay.innerHTML = `
@@ -238,17 +187,19 @@
 
             if (latestData.status === "poli") {
                 noAntrianDisplay.innerHTML = `
-        <span id='no_antrian_latest' class="text-9xl font-bold text-yellow-300">${latestData.no_antrian}</span>
-        <span id='no_poli_latest' class="text-3xl font-medium">${latestData.no_poli}</span>
-    `;
+                    <span id='no_antrian_latest' class="text-9xl font-bold text-yellow-300">${latestData.no_antrian}</span>
+                `;
+                tujuanDisplay.innerHTML = `
+                    <span id='no_poli_latest' class="text-8xl font-medium">${latestData.no_poli}</span>
+                `;
             } else if (latestData.status === "rekam medis") {
                 noAntrianDisplay.innerHTML = `
-        <span id='no_antrian_rm_latest' class="text-9xl font-bold text-yellow-300">${latestData.no_antrian_rm}</span>
-    `;
+                    <span id='no_antrian_rm_latest' class="text-9xl font-bold text-yellow-300">${latestData.no_antrian_rm}</span>
+                `;
             } else {
                 noAntrianDisplay.innerHTML = `
-        <span class="text-xl font-medium">Tidak ada status</span>
-    `;
+                    <span class="text-xl font-medium">Tidak ada status</span>
+                `;
             }
 
             // Loop untuk menampilkan 5 data di riwayat_antrian
